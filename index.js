e83caf8801779bf2c100175aeaa85ebc3b6de21a
guxuelong <15921589007@qq.com>
2016-04-14 18:59:43 +0800

    添加针对带参数请求的支持

--- index.js
+++ index.js
@@ -8,10 +8,19 @@ var Mock = require('./lib/mock'),
 	mocksource = require('../../zamock.config')
 
 var ZaMock = function(host) {
+
+
+	// host 针对可配置host 假如不设置的话，取当前host
+	host = host || window.location.origin;
+	// 读取配置文件 获取已经配置的请求mock数据
 	for (var key in mocksource) {
+		// 循环mock数据，创建拦截列表
 		var url = host + '\/' + key.replace('_', '\/');
-		console.log('url:' + url);
-		Mock.mock(new RegExp(url), mocksource[key]);
+		// 支持带参数的请求，但是同时设置两种的话不带参数的会覆盖带参数的请求
+		if (url.indexOf('?') < 0) {
+			url = new RegExp(url);
+		}
+		Mock.mock(url, mocksource[key]);
 	}
 }
 module.exports = ZaMock;
